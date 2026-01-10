65299ef09657042e36bebe4e8c4fe72b85da7018
Jacob O'Mara <Jacob.omara@openfifth.co.uk>
2026-01-10 21:06:29 +0000

    fix: correct helpdesk endpoint names to use ticket instead of helpdesk
    API uses get_ticket_list/get_ticket_list_count/edit_ticket,
    not get_helpdesk_list/get_helpdesk_list_count/edit_helpdesk

--- src/opencrm/resources/helpdesk.py
+++ src/opencrm/resources/helpdesk.py
@@ -4,8 +4,8 @@ from opencrm.resources.base import BaseResource
 
 class HelpdeskResource(BaseResource[Helpdesk]):
     _module_name = "Helpdesk"
-    _list_endpoint = "get_helpdesk_list"
-    _count_endpoint = "get_helpdesk_list_count"
+    _list_endpoint = "get_ticket_list"
+    _count_endpoint = "get_ticket_list_count"
     _get_endpoint = "get_ticket"
-    _edit_endpoint = "edit_helpdesk"
+    _edit_endpoint = "edit_ticket"
     _model_class = Helpdesk
